d4deff323d2c0e776a34d6114ca51cac1be5cc35
arishkinnov777 <arina-novikova2009@yandex.ru>
2024-01-06 20:46:55 +0300

    Ex5: Парсинг JSON

--- src/test/java/ru/arina/novikova/learn/HelloWorldTest.java
+++ src/test/java/ru/arina/novikova/learn/HelloWorldTest.java
@@ -1,8 +1,13 @@
 package ru.arina.novikova.learn;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class HelloWorldTest {
 
@@ -12,5 +17,15 @@ public class HelloWorldTest {
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    @DisplayName("Ex5: Парсинг JSON")
+    public void testJsonParsing() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        List<LinkedHashMap<String, String>> messages = response.getList("messages");
+        System.out.println(messages.get(1).get("message"));
     }
 }
